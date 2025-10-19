c2d103384014e7812701dc2b5bf6889ecb9f641c
Pehan-janu <pehanjanu@gmail.com>
2025-10-20 00:52:48 +0530

    Initial Laravel project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="lofi">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>ChiperX Home</title>
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.51.5/dist/full.css" rel="stylesheet" type="text/css" />
+    
