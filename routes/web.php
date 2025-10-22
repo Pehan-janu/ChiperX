a8038db922f15f1703a9c18ea2f12bb122b96b06
Pehan-janu <pehanjanu@gmail.com>
2025-10-22 10:08:25 +0530

    Update web.php file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,43 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChiperController;
+use App\Http\Controllers\Auth\Register;
+use App\Http\Controllers\Auth\Logout;
+use App\Http\Controllers\Auth\Login;
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/', [ChiperController::class, 'index']);
+
+//protected routes|
+
+Route::middleware('auth')->group(function(){
+
+    Route::post('/chiper', [ChiperController::class, 'store']);
+    Route::get('/chiper/{chiper}/edit', [ChiperController::class, 'edit']);
+    Route::put('/chiper/{chirp}', [ChiperController::class, 'update']);
+    Route::delete('/chiper/{chiper}', [ChiperController::class, 'destroy']);
+
 });
+
+
+// Registration routes
+Route::view('/register', 'auth.register')
+    ->middleware('guest')
+    ->name('register');
+ 
+Route::post('/register', Register::class)
+    ->middleware('guest');
+
+ 
+// Logout route
+Route::post('/logout', Logout::class)
+    ->middleware('auth')
+    ->name('logout');
+
+    // Login routes
+Route::view('/login', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+ 
+Route::post('/login', Login::class)
+    ->middleware('guest');
