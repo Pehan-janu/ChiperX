67ff7597025ca8acd6bc0701c6952f4be25e95ea
Pehan-janu <pehanjanu@gmail.com>
2025-10-22 12:15:27 +0530

    Add chiper relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Chiper;
 
 class User extends Authenticatable
 {
@@ -44,5 +46,14 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+
+    /**
+     * Get the chirps for the user.
+     */
+    public function chiper(): HasMany
+    {
+        return $this->hasMany(Chiper::class);
     }
 }
